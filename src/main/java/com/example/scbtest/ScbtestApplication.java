71a1a12b180a57990526307bd85650db7e9baf66
UlrikaMyAlva <ulrika.m.a.eriksson@gmail.com>
2023-08-16 00:45:48 +0200

    Initial commit

---
+++ src/main/java/com/example/scbtest/ScbtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.scbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScbtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scbtest/ScbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
